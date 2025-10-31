d9f2e68fa2847f4f5deda093ab7fe11009929e15
MD Faizan Alam <alamf015@gmail.com>
2025-11-01 00:36:50 +0530

    instagram update changes and added more features

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ const Card = (props) => {
             <div className="col-12 col-md-4 my-2">
                 <div className="card">
                     <div className="card-body">
-                        <h6 className="card-title">Username: {props.user.string_list_data[0].value}</h6>
+                        <h6 className="card-title">Username: {props.user.title ? props.user.title:props.user.string_list_data[0].value}</h6>
                         <p className="card-text text-muted">{props.cardMessage + date.toLocaleString()}</p>
                         <a target='_blank' rel='noreferrer' href={props.user.string_list_data[0].href} className="btn btn-warning">View Profile</a>
                     </div>
